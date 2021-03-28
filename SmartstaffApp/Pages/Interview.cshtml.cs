42cae2a3d9126e57b141c5415f1019a3f4a21afd
krakaz238@gmail.com <krakaz238@gmail.com>
2021-03-28 19:25:22 +0300

    feature: Изменяет способ обработки фильтра на странице интервью

--- SmartstaffApp/Pages/Interview.cshtml.cs
+++ SmartstaffApp/Pages/Interview.cshtml.cs
@@ -1,9 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.Extensions.Logging;
 using SmartstaffApp.Models;
 using SmartstaffApp.Services;
@@ -16,17 +16,29 @@ namespace SmartstaffApp.Pages
         private readonly IStaffService staffService;
 
         public IList<DetailInformationByMonth> DetailInformationByMonth { get; set; }
+        public List<SelectListItem> SignificantStatus { get; set; } = new List<SelectListItem>()
+            {
+                new SelectListItem { Value = "0", Text = "Все" },
+                new SelectListItem { Value = "1", Text = "Значимые" },
+            };
+
+        public InterviewFilter Filter { get; set; } = new InterviewFilter();
 
         public InterviewModel(ILogger<IndexModel> logger, IStaffService staffService)
         {
             this.logger = logger;
             this.staffService = staffService;
         }
-        public async Task OnGet(int isSignificant, CancellationToken cancellationToken)
+        public async Task OnGet(InterviewFilter filter, CancellationToken cancellationToken)
         {
             var year = DateTime.Now.Year;
-            var result = await this.staffService.GetDetailInformationByMonthAsync(Convert.ToBoolean(isSignificant), year, cancellationToken);
+            var result = await this.staffService.GetDetailInformationByMonthAsync(Convert.ToBoolean(filter.SignificantStatusId), year, cancellationToken);
             this.DetailInformationByMonth = result;
         }
+    }
+
+    public class InterviewFilter
+    {
+        public int SignificantStatusId { get; set; }
     }
 }
